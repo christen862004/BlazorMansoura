cb0b718a0538544828f364f70c40d59666cc3d22
Christen <christen.foad@gmail.com>
2024-05-07 10:57:25 +0300

    Add project files.

---
+++ BlazorMansoura/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorMansoura
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            
+            //display conent
+            builder.RootComponents.Add<App>("#app");
+            //Change title
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+            //inject Day3
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
